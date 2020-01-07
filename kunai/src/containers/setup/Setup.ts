205fb6dbada6abaab124227be79e296d050c00d0
GerryLuo <dkluohanga@163.com>
2020-01-07 18:01:11 +0800

    加上Setup的皮肤，感觉一股浓浓的山寨味

--- kunai/src/containers/setup/Setup.ts
+++ kunai/src/containers/setup/Setup.ts
@@ -1,10 +1,38 @@
-class Setup extends egret.Sprite {
+/*
+ * @Author: your name
+ * @Date: 2019-12-23 12:12:04
+ * @LastEditTime : 2020-01-07 17:46:18
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \weagentclient\kunai\src\containers\setup\Setup.ts
+ */
+
+class Setup extends eui.ItemRenderer {
     constructor() {
         super()
         this.init()
     }
 
     private init() {
+        // 创建场景
+        this.createScene()
+        // 加载数据
+        this.loadData()
+    }
 
+    private createScene() {
+        this.skinName = "resource/eui_skins/SetupSkin.exml";
+
+        const { stage } = egret.MainContext.instance
+
+        this.width = stage.stageWidth
+        this.height = stage.stageHeight - 90
+    }
+
+    private loadData() {
+        // Http.get(API.ApiDataEntrance).then(res => {
+        //     // unknown转any
+        //     var rsp: any = res
+        // })
     }
 }
